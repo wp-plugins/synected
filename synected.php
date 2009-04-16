cd13d7f9df6f9db6aca1dbc65b995a03d9171bad
jeff.smith <jeff.smith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-16 14:35:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/synected/trunk@111037 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ synected.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name:Synected
+Plugin URI:http://www.blurbia.com/plugins/synected/
+Description: Creates branded shortened URLs, a la tinyurl.com or bit.ly.
+Version: 0.8 beta
+Author: Blurbia
+Author URI: http://www.blurbia.com/
+*/
+
+/*  Copyright 2009 Jeff Smith (email: jeff@blurbia.com)
+
+    This file is part of Synected.
+
+    Synected is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Synected is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Synected.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+require_once('synected.class.php');
+?>
